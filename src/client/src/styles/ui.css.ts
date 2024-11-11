e635b6f3af016212d48256dc4fcb406e3daec809
raustin9 <raustin9@vols.utk.edu>
2024-11-11 19:42:03 +0000

    try again

---
+++ src/client/src/styles/ui.css.ts
@@ -0,0 +1,13 @@
+import { style } from "@vanilla-extract/css";
+
+export const textarea = style({
+    width: '80%',
+    
+    maxWidth: '80%',
+    padding: '10px',
+    fontSize: '16px',
+    border: '2px solid #ccc',
+    borderRadius: '5px',
+    outline: 'none',
+    transition: 'border-color 0.3s ease',
+})
